6adb4556e47b6bc871438741811f5fd31f604798
Néstor Triana <ngtriana12@gmail.com>
2023-05-03 20:50:34 -0500

    se crea comunicación entre componentes

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -12,7 +12,7 @@ export const GifExpertApp = () => {
   return (
     <>
         <h2>GifExpertApp</h2>
-        <AddCategory />
+        <AddCategory setCategories={ setCategories } />
         <hr />
 
         <ol>
